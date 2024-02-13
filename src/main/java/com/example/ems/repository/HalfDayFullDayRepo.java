7c7367d6bfaa95826c223df68e60d0cd218b93ab
dilipSunar123 <rexsunar37010@gmail.com>
2024-02-13 23:20:19 +0530

    Deleted unnecessary tables(files)

--- src/main/java/com/example/ems/repository/HalfDayFullDayRepo.java
+++
@@ -1,6 +0,0 @@
-package com.example.ems.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface HalfDayFullDayRepo extends JpaRepository<HalfDayFullDayEntity, Integer> {
-}
