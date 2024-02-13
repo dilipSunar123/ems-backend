e99abbfa5e08c0769694a62eb012cc63fa3d19d7
dilipSunar123 <rexsunar37010@gmail.com>
2024-02-13 23:19:30 +0530

    Deleted unnecessary tables(files)

--- src/main/java/com/example/ems/repository/FirstHalfSecondHalfRepo.java
+++ src/main/java/com/example/ems/repository/FirstHalfSecondHalfRepo.java
@@ -1,6 +1,5 @@
 package com.example.ems.repository;
 
-import com.example.ems.entity.FirstOrSecondHalfEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FirstHalfSecondHalfRepo extends JpaRepository<FirstOrSecondHalfEntity, Integer> {
